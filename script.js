add96634ff55713fd68fd51441c355d53034e449
SreeHari1206 <araveticharan29882@gmail.com>
2026-06-11 18:54:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Project Overlay
-
 function openProjects() {
 
     document.getElementById("projectOverlay").style.display = "flex";
-
     document.body.style.overflow = "hidden";
 
 }
@@ -11,80 +8,6 @@ function openProjects() {
 function closeProjects() {
 
     document.getElementById("projectOverlay").style.display = "none";
-
     document.body.style.overflow = "auto";
 
 }
-
-
-// Typing Animation
-
-const words = [
-
-    "Structural FEA",
-
-    "Multibody Dynamics",
-
-    "Dynamic Analysis",
-
-    "Engineering R&D"
-
-];
-
-let wordIndex = 0;
-
-let charIndex = 0;
-
-const typingText = document.getElementById("typing-text");
-
-
-function type() {
-
-    if (charIndex < words[wordIndex].length) {
-
-        typingText.textContent += words[wordIndex].charAt(charIndex);
-
-        charIndex++;
-
-        setTimeout(type, 100);
-
-    }
-
-    else {
-
-        setTimeout(erase, 1500);
-
-    }
-
-}
-
-
-function erase() {
-
-    if (charIndex > 0) {
-
-        typingText.textContent = words[wordIndex].substring(0, charIndex - 1);
-
-        charIndex--;
-
-        setTimeout(erase, 50);
-
-    }
-
-    else {
-
-        wordIndex++;
-
-        if (wordIndex >= words.length) {
-
-            wordIndex = 0;
-
-        }
-
-        setTimeout(type, 500);
-
-    }
-
-}
-
-type();
